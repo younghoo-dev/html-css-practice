933a43a401addbd11f012577f861d9a3ecc33a8e
younghoo-dev <kingdudgn@gmail.com>
2026-02-27 16:46:18 +0900

    JavaScript 자료형 연습

---
+++ main.js
@@ -0,0 +1,17 @@
+var num = 10;
+console.log(num)
+
+let des = 15;
+console.log(des)
+
+const name = "Younghoo"
+console.log(`Hello ${name}`)
+
+let string = '문자열은 큰따옴표("")나 작은따옴표(' + "'')로 감싸면 됩니다."
+console.log(string);
+
+let box = 10 > 20;
+console.log(box);
+
+let num1 = [90, 88, 89, 95, 96];
+console.log(num1[0]);
