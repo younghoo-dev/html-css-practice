0ab3e2157a81ac4cb3202a05038ca2649ad0d327
younghoo-dev <kingdudgn@gmail.com>
2026-03-01 21:23:40 +0900

    JavaScript 구구단 출력

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let sum = 0
-for(let i = 1; i <= 100; i++){
-    sum = sum + i
+for(i = 1; i < 10; i++){
+    for(k = 1; k < 10; k++){
+        console.log(`${i} x ${k} = ${i*k}`);
+    }
 }
-console.log(sum);
