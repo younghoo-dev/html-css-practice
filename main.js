3397f30b58c3577d555d8b91da7c3ce6781bb9ae
younghoo-dev <kingdudgn@gmail.com>
2026-03-01 21:06:21 +0900

    JavaScript 1~100까지 더하는 코드

--- main.js
+++ main.js
@@ -1,27 +1,5 @@
-num = 1;
-while(num <= 10){
-    console.log(num);
-    num++;
+let sum = 0
+for(let i = 1; i <= 100; i++){
+    sum = sum + i
 }
-
-do{
-    console.log('무조건')
-    console.log('한 번은 실행')
-}while(false);
-
-for(let a = 1; a <= 2; a++){
-    console.log(`a= ${a}`);
-    for(let b = 1; b < 4; b++){
-        console.log(`b= ${b}`)
-    }
-}
-
-const arr = ['apple','banana', 'orange']
-for(let i = 0; i < arr.length; i++){
-    console.log(arr[i]);
-}
-
-for(let c = 1; c < 12; c++){
-    if(c % 2 === 0) continue
-    console.log(c)
-}
+console.log(sum);
