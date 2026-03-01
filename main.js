8a4517d9ea2567ed772caad436f199ce41199b4d
younghoo-dev <kingdudgn@gmail.com>
2026-03-01 20:14:03 +0900

    JavaScript 조건문 다루기

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
-let x = 10;
-let y = 5;
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
-console.log(x%y);
-console.log(x**y);
+let num = 7
+if(num % 2 === 0){
+    console.log('변수 num에 할당된 숫자는 짝수입니다.');
+}
+else{
+    console.log('변수 num에 할당된 숫자는 홀수입니다.');
+}
 
-let ax = ++x;
-console.log(ax);
+let score = 79
+if(score > 90 && score <= 100){
+    console.log('A+')
+}
+else if(score <= 90 && score >80){
+    console.log('B+')
+}
+else{
+    console.log('바보')
+}
 
-let bx = --x;
-console.log(bx);
 
-let cx = -x;
-console.log(cx)
+let food = 'carrot'
+switch(food){
+    case 'melon':
+    case 'apple':
+    case 'banana':
+        console.log('fruit');
+        break
 
-let z = '10'
+    case 'carrot':
+        console.log('vegetable')
+        break
 
-console.log(x==z);
-console.log(x===z);
-
-let score = 90;
-let grade = score >= 90 ? 'A+' : 'B';
-console.log(grade);
+    default:
+        console.log("It's not a fruit and vegetable.")
+}
