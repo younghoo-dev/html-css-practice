cf8fd85002b255b92472fb5e677089b1d5fda93c
younghoo-dev <kingdudgn@gmail.com>
2026-03-01 21:38:29 +0900

    JavaScript 암스트롱 수 찾기 코드

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-for(i = 1; i < 10; i++){
-    for(k = 1; k < 10; k++){
-        console.log(`${i} x ${k} = ${i*k}`);
+for(a = 0; a < 10; a++){
+    for(b = 0; b < 10; b++){
+        for(c = 0; c < 10; c++){
+            if(a**3 + b**3 + c**3 === a*100 + b*10 + c){
+                console.log(a*100 + b*10 + c)
+            }
+        }
     }
 }
