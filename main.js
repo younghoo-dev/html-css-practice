6420e93c80305343f0840657227b58bdff10944a
younghoo-dev <kingdudgn@gmail.com>
2026-03-04 16:39:33 +0900

    JavaScript 함수 기능

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-for(a = 0; a < 10; a++){
-    for(b = 0; b < 10; b++){
-        for(c = 0; c < 10; c++){
-            if(a**3 + b**3 + c**3 === a*100 + b*10 + c){
-                console.log(a*100 + b*10 + c)
-            }
-        }
+/*function gugudan(){//함수 시작
+for(let i = 1; i<=9; i++){
+    console.log(`3 x ${i} = ${3 * i}`);
+}
+} //함수 끝
+
+gugudan();
+
+
+const gugudan5 = () => {
+    for(let i = 1; i <=9; i++){
+        console.log(`5 x ${i} = ${5 * i}`);
     }
 }
+gugudan5();
+
+
+const gugudanx = (wow) => {
+    for(let i = 1; i <=9; i++){
+        console.log(`${wow} x ${i} = ${wow * i}`);
+    }
+}
+gugudanx(7);*/
+
+
+function sum(num1, num2){
+    const result = num1 + num2
+    return result
+}
+const result1 = sum(10, 20);
+const result2 = sum(30, 40);
+
+console.log(result1 + result2);
